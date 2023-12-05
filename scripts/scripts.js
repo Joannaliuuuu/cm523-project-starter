a75c65cc82257c63d642851633cc910aa2ed8a81
Joanna Liu <zlliu@bu.edu>
2023-12-05 10:06:37 -0500

    2nd

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,21 @@
 /* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
+let flx = document.getElementById("flex")
+
+function navlist() {
+    flx.style.right = "0";
+}
+
+function out() {
+    flx.style.right = "-20vw"
+}
+
+function navlist() {
+    flx.style.right = "0";
+}
+
+function forward(){
+    history.forward();
+}
+function back(){
+    history.back();
+}
